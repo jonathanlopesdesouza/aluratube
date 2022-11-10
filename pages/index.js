a1d50b519b5fef13902a13982486b161e62ff833
Jonathan Lopes <jonathanlopesdesouza@hotmail.com>
2022-11-10 14:31:55 -0300

    DarkModeTheme

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import {CSSReset} from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
-function HomePage() {
-    const estilosDaHomePage = {
-        //backgroundColor: "red"
-    };    
+function HomePage() {   
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
     
       return (
         <>
-        <CSSReset />   
+        
         <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -42,6 +38,7 @@ function HomePage() {
     //}
 
   const StyleHeader = styled.div`
+        background-color: ${(theme)=> theme.backgroundLevel1 };
         img{
             width: 80px;
             height: 80px;
